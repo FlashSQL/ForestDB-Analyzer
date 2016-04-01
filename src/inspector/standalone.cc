5b778b300735bc3b44ac626056205e4dc0228c14
wurikiji <wurikiji@gmail.com>
2016-04-01 14:04:28 +0900

    Add option parser for inspector
    - Now, FDB inspector gets -b and -d options for execution.
        - "-b" option: blocksize of a target forestdb
        - "-d" option: device name of a target blktrace

--- src/inspector/standalone.cc
+++ src/inspector/standalone.cc
@@ -15,11 +15,17 @@
 #include <inttypes.h>
 #include <unistd.h>
 
-using namespace std;
+
+#define SUCCESS         0x00
+#define UNKNOWN_ERROR   0x01
+#define UNKNOWN_ARG     0x02 
+#define DEV_NOT_FOUND   0x04
 
 void print_usage(char* name) {
-    printf("Usage: %s blocksize\n", name);
+    printf("Usage: %s -b blocksize -d devname\n", name);
     printf("blocksize: block size of target forestdb\n");
+    printf("devname: Device name which contains target forestdb\n");
+    printf("         (/dev/sdxx)\n");
 }
 
 /**
@@ -34,17 +40,91 @@ struct config global_config;
 
 void init_config(void) {
     global_config.blocksize = 0;
-    global_config.devname = NULL;
+    global_config.devname = (char*)malloc(sizeof(char) * 512);
 }
+
+void destroy_config(void) {
+    free(global_config.devname);
+}
+
+int verify_config(void) {
+    if(0 >= global_config.blocksize) {
+        global_config.blocksize = 4096;
+    }
+    if(0 >= strlen(global_config.devname)) {
+        return DEV_NOT_FOUND;
+    }
+
+    printf("====== Configurations ======\n");
+    printf("Blocksize: %" PRIu64 " bytes\n", global_config.blocksize);
+    printf("Device name: %s\n", global_config.devname);
+    printf("============================\n");
+    return SUCCESS;
+}
+
+/**
+  * Block marker of forestdb
+  * (commit version is 671823e363615f37439f3ddaaeb730018a3db0a4) 
+  *
+  * #define BLK_MARKER_BNODE (0xff)
+  * #define BLK_MARKER_DBHEADER (0xee)
+  * #define BLK_MARKER_DOC (0xdd)
+  * #define BLK_MARKER_SB (0xcc) // superblock
+  * #define BLK_MARKER_SIZE (1)
+  * #define DOCBLK_META_SIZE (16)
+  * struct docblk_meta {
+  *     bid_t next_bid;
+  *     uint16_t sb_bmp_revnum_hash;
+  *     uint8_t reserved[5];
+  *     uint8_t marker;
+  * };
+  */
 
 int do_inspect(void) {
+    return SUCCESS;
 }
 
+/** 
+  * extern int optind, opterr, optopt;
+  * extern char *optarg;
+  */
+int parse_opt(int argc, char* argv[]) {
+    const char *opt = "b:d:";
+    char option;
+    
+    optind = 1;
+    while ( -1 != (option = getopt(argc, argv, opt)) ) {
+        switch (option) {
+            case 'b' :
+                global_config.blocksize = atoi(optarg);
+                break;
+            case 'd' : 
+                strcpy(global_config.devname, optarg);
+                break;
+            default:
+                return UNKNOWN_ARG;
+        };
+    };
+    return SUCCESS;
+}
 int main(int argc, char *argv[]) {
     int rv;
 
     init_config();
+
+    if( SUCCESS != (rv = parse_opt(argc, argv)) ) {
+        print_usage(argv[0]);
+        goto exit;
+    }
+
+    if( SUCCESS != (rv = verify_config()) ) {
+        print_usage(argv[0]);
+        goto exit;
+    }
+
     rv = do_inspect();
 
+exit:
+    destroy_config();
     return rv;
 }
